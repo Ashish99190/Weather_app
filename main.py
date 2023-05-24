ffd50d60f7214f96838cb231e5b323fbb6a7643a
Ashish99190 <128565425+Ashish99190@users.noreply.github.com>
2023-05-24 12:07:10 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import requests
 from twilio.rest import Client
-api_key = "74999d3770957deb65cc902cd2d899d7"
+api_key = "your_key"
 end_url = "https://api.openweathermap.org/data/2.8/onecall"
-account_ssid = "ACd9fb3b717abd8f9e0f4741407e9575b2"
-acct_token ="8b22d8303fa7a139b4f4ffaa29a66d42"
+account_ssid = "yout_id"
+acct_token ="your_token"
 parameters = {
     "lon": 77.481400,
     "lat": 20.491600,
@@ -26,7 +26,7 @@ if rain:
     client = Client(account_ssid, acct_token)
     message = client.messages.create(
         body="it's going to be rain today, remember to bring an umbrella.",
-        from_="+13204139003",
+        from_="your_no",
         to="+818097708193"
     )
     print(message.status)
